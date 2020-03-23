f873060d3c4f7fbdc55d0350e0780c0df4935088
oteklund <ottoeklund@gmail.com>
2020-03-23 11:12:21 +0200

    Add descriptions to components for better legibility, add new components

---
+++ src/components/chat-window/ChatOutput.tsx
@@ -0,0 +1,18 @@
+/*
+This component displays previously sent and received chats.
+*/
+import React from 'react'
+
+interface Props {
+    
+}
+
+const ChatOutput = (props: Props) => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default ChatOutput
---
+++ src/components/chat-window/ChatParticipants.tsx
@@ -0,0 +1,18 @@
+/*
+This component displays the participant names and pictures in the chat window.
+*/
+import React from 'react'
+
+interface Props {
+    
+}
+
+const ChatParticipants = (props: Props) => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default ChatParticipants
