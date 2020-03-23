ceda3c8212febb310ab1eafb341499b5d4cdcda9
Pekka Härkönen <pekka.o.harkonen@gmail.com>
2020-03-23 14:17:00 +0200

    basic structure for reducers done

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+import { Provider } from 'react-redux';
+import store from './store';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Provider store={store}>
+      <div className='App'></div>
+    </Provider>
   );
 }
 
--- src/reducers/items.ts
+++ src/reducers/items.ts
@@ -1,39 +1,14 @@
-import {
-  ADD_ITEM,
-  EDIT_ITEM,
-  DELETE_ITEM,
-  SET_ITEMS,
-  ItemActionTypes
-} from '../types/actions';
-import { Item } from '../types/Item';
+import { IItem, IFetchItemActions } from '../actions';
+import { ActionTypes } from '../actions/types';
 
-const initialState: Item[] = [];
-
-const itemsReducer = (
-  state = initialState,
-  action: ItemActionTypes
-): Item[] => {
+export const itemsReducer = (
+  state: IItem[] = [],
+  action: IFetchItemActions
+) => {
   switch (action.type) {
-    case ADD_ITEM:
-      return [...state, action.item];
-    case EDIT_ITEM:
-      return state.map(item => {
-        if (item.id === action.item.id) {
-          return {
-            ...item,
-            ...action.item
-          };
-        } else {
-          return item;
-        }
-      });
-    case DELETE_ITEM:
-      return state.filter(({ id }) => id !== action.id);
-    case SET_ITEMS:
-      return action.items;
+    case ActionTypes.fetchItems:
+      return action.payload;
     default:
       return state;
   }
 };
-
-export { itemsReducer };
