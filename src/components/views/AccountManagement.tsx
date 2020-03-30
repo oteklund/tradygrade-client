db66293cad7f60a7943b1cf3a0be2b74530a1df4
oteklund <ottoeklund@gmail.com>
2020-03-30 21:09:01 +0300

    first version of landing completed

--- src/components/views/AccountManagement.tsx
+++ src/components/views/AccountManagement.tsx
@@ -1,9 +1,10 @@
 /*
 This component is the view where users can manage account details such as username, display image etc. Users can log out or terminate accounts here. Also displays items that the user currently has for sale. 
 */
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { StoreState, User } from '../../models/types';
 import { connect } from 'react-redux';
+import "./AccountManagement.scss"
 
 interface Props {
     user: User
@@ -11,25 +12,41 @@ interface Props {
 
 const AccountManagement = ({ user }: any) => {
     
-    const [userData, setUserData] = useState("")
+    const [userData, setUserData] = useState({
+        name: user.name,
+        email: user.email,
+        image_url: user.image_url
+    })
 
     const getUserData = async () => {
         return (
-            await fetch("http://localhost:4000/api/users/22", {
+            await fetch(`http://localhost:4000/api/users/${user.id}`, {
                 method: "GET",
                 headers: {
                     "Authorization": "Bearer " + user.token
                 }
             })
             .then((res: any) => res.json())
-            .then((data: any) => setUserData(data.name))
+            .then((data: any) => setUserData(data))
         )
     }
 
+    const openEditUserData = () => {
+        //logic
+    }
+
+    // useEffect(() => {
+    //     getUserData()
+    //     //eslint-disable-next-line
+    // }, [])
+
     return (
-        <div>
-            <button onClick={getUserData}>get data</button>
-            <span>{userData}</span>
+        <div className="account-management-container">
+            <span>{user.name}</span>
+            <br/>
+            <span>{user.email}</span>
+            <br/>
+            <img src="image_url" alt="user"/>
         </div>
     )
 }
