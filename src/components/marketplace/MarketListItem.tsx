1715c57e1f1afd0ed705a40fbcac43fbe6ab2641
Henri <henri.kasvand@gmail.com>
2020-03-30 23:15:58 +0300

    SCSS updated- dashboard, marketplace, new item

--- src/components/marketplace/MarketListItem.tsx
+++ src/components/marketplace/MarketListItem.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import { Item } from '../../models/types';
+import moment from 'moment';
 
 interface Props {
   item: Item;
 }
 
 const MarketListItem = ({ item }: Props) => {
-  return <div>{item.item.name}</div>;
+  return <div className="marketItem">
+  <p>{item.item.name}/ {item.item.price}€</p>
+  <img className="itemPicture" src={item.item.pictureURL} height="100px" />
+  <p>Listed By: {item.seller.name}</p>
+  <p className="dashDate" >{moment(item.item.listedAt).format('DD-MM-YYYY')}</p>
+</div>;
 };
 
 export default MarketListItem;
