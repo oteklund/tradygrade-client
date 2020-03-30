b82360ce48d99865dac2519a988982ff310f3d34
Pekka Härkönen <pekka.o.harkonen@gmail.com>
2020-03-30 16:17:29 +0300

    dashboard now showing 5 most recently added items

--- src/components/views/Dashboard.tsx
+++ src/components/views/Dashboard.tsx
@@ -2,126 +2,44 @@
 This component makes up the front page of the site. Users who have logged on are redirected here when accessing the root url.
 */
 import './Dashboard.scss';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { StoreState, Item, User } from '../../models/types';
+import { connect } from 'react-redux';
+import moment from 'moment';
 
-interface Props {}
+interface Props {
+  user: User | undefined;
+  items: Item[];
+}
 
-const Dashboard = (props: Props) => {
+const Dashboard = ({ user, items }: Props) => {
   return (
     <div className='dashboard'>
-      <div>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium
-        eaque sequi fuga natus vel eveniet modi, aperiam fugit, nesciunt iste
-        facere reprehenderit provident harum officia consequuntur ducimus
-        dignissimos necessitatibus impedit exercitationem hic, saepe recusandae
-        consectetur iusto dolorem! Obcaecati eaque itaque doloremque, deserunt
-        autem sint voluptatem fuga odit laboriosam debitis, reiciendis sunt
-        molestiae accusamus. Facilis nobis architecto sapiente necessitatibus
-        molestias illo optio enim minima voluptatem ipsam. Quod soluta totam
-        quaerat ad, id nisi cupiditate sed magnam earum illo, eligendi iste
-        nihil perspiciatis, aliquid ratione? Aperiam veniam, dolorum fuga
-        similique reprehenderit incidunt, dolores odit delectus accusamus
-        mollitia amet? Soluta vitae, eius culpa ducimus omnis temporibus et a!
-        Iste debitis ad, dolores quasi numquam nihil nam deleniti commodi
-        perspiciatis voluptas autem magni ipsum ut, quis incidunt, ratione nisi.
-        Similique dolorem illum cumque cupiditate ut quidem aut fugit ab modi
-        porro qui possimus perferendis reiciendis mollitia quae omnis sapiente,
-        rerum sint? Dolorum laborum autem alias esse porro distinctio nobis
-        labore, dolorem modi similique assumenda vel sequi non magnam enim
-        beatae amet voluptatem! Repellendus aspernatur est itaque dignissimos
-        incidunt, iste doloribus. Facilis earum quam explicabo ad aspernatur
-        cumque enim cupiditate veritatis adipisci hic sit similique fugiat eos
-        saepe nobis facere maxime, vel ab beatae pariatur officia dolorum,
-        reprehenderit expedita ex? Explicabo eum modi itaque error labore eius
-        necessitatibus placeat consectetur, atque, non maiores! Dolore ipsum
-        quasi consequatur alias natus et sapiente. Dignissimos exercitationem
-        debitis ad voluptatem error blanditiis expedita aspernatur quisquam
-        voluptatibus ratione quo soluta commodi dolor amet, cumque reprehenderit
-        incidunt. Est voluptate incidunt unde nesciunt, aspernatur repellendus
-        veritatis architecto a pariatur necessitatibus dicta consectetur enim
-        labore rem saepe adipisci sapiente ea consequatur reprehenderit beatae.
-        Eligendi laudantium quod quis necessitatibus enim vitae voluptas,
-        temporibus neque facere quam! Voluptatem nisi, labore, maxime debitis
-        illo repudiandae vero commodi provident accusamus nulla inventore, eaque
-        culpa repellendus vel numquam. Corporis, omnis. Eligendi, minima unde,
-        culpa, officiis aspernatur impedit perferendis libero facere porro
-        voluptatum id? Placeat, adipisci ut enim perferendis quod sit veritatis
-        at corporis aperiam odit quae doloribus dolores neque sed aut
-        repellendus blanditiis error beatae. Molestiae iste quae quia quam,
-        sequi ullam minus neque quas consequuntur unde fugiat praesentium
-        recusandae dolore voluptates reprehenderit debitis aspernatur totam
-        provident quis, sit ea? Itaque eius sunt, aliquam ipsa suscipit optio
-        nihil vero nostrum, fugiat, sed ut! Esse, sapiente. Fugiat, alias fuga
-        at quibusdam magnam eos rerum obcaecati maiores tempora! Officiis
-        cupiditate nesciunt eaque id asperiores quas cum consectetur illo
-        ratione accusamus laborum, maxime itaque impedit ea adipisci harum nulla
-        blanditiis recusandae rerum officia, labore cumque. Dolorem sit quam
-        ratione qui modi mollitia pariatur cum incidunt ab nam, eos esse vel
-        totam architecto debitis nulla? Enim nemo perferendis facere, labore
-        eveniet, ipsam, modi vero recusandae soluta corporis exercitationem nam
-        illo sint vel architecto minima dolore! Praesentium ipsa magni nulla
-        neque suscipit possimus harum voluptatem? Deleniti debitis error
-        pariatur natus fugiat odit laboriosam, itaque dolores ab omnis iusto id,
-        consectetur autem a, praesentium veritatis deserunt? Consectetur
-        doloremque sed alias sint. Atque aliquam ab tenetur optio asperiores
-        sequi eaque totam dicta reiciendis ex, cupiditate impedit enim
-        repellendus, cum expedita vel dolores labore aliquid tempore nostrum,
-        fugiat nobis accusamus tempora. Modi exercitationem molestias dolor sed.
-        Porro itaque repellat minus quisquam eum similique eveniet laudantium
-        animi eos ex accusamus aliquid, modi architecto, aperiam voluptatibus
-        nihil impedit reiciendis harum. Sit odit consectetur sequi atque
-        consequatur fugit iusto commodi laboriosam dolorum ratione voluptas quis
-        cumque nostrum a ab autem, totam recusandae? Perspiciatis id at, et
-        totam accusantium assumenda, modi, rem nam quis eum impedit voluptate
-        asperiores accusamus veniam blanditiis porro fuga nesciunt iusto
-        deleniti hic? Est repellat, ad magnam corrupti natus enim possimus ipsum
-        nam. Doloremque blanditiis quaerat et autem odit enim, optio tenetur
-        nobis, unde impedit illum similique repellat dicta doloribus temporibus
-        alias tempore mollitia, id adipisci sunt hic. Illum odit fuga architecto
-        deleniti laboriosam tempore? Suscipit minima molestiae corporis. Magni
-        optio a, mollitia perspiciatis reprehenderit nostrum ratione cupiditate,
-        minus in animi totam culpa, aspernatur inventore veritatis similique
-        debitis repellendus repellat deleniti. Dolores, aspernatur cupiditate ea
-        architecto nobis, officia dolorum tempore fugiat beatae perferendis,
-        molestias quidem cum labore ipsum quam culpa facere? Hic voluptas
-        consequatur iure impedit deleniti sed nesciunt, natus obcaecati
-        voluptatibus ad minus dolor quo pariatur laudantium sint error odio
-        nostrum ducimus! Neque dolorum, tenetur optio, placeat odit iste
-        laudantium perspiciatis est odio voluptatum, ducimus alias sit
-        praesentium ratione. Deleniti reprehenderit, at voluptatem doloribus vel
-        totam quia nam voluptate dolores, reiciendis sit ab ullam qui ipsum
-        quisquam? Fugit tenetur hic quaerat porro nobis alias laboriosam quidem
-        odio excepturi, placeat ullam velit? Eos, odio perferendis! Nobis
-        dolorum repudiandae atque exercitationem voluptas temporibus assumenda
-        fuga ipsa, eveniet saepe nam, modi sunt ex accusamus! Libero quasi
-        consequuntur delectus quae harum distinctio asperiores nisi ab cum
-        accusantium quos dolores, quas at amet labore ratione velit deserunt
-        officiis debitis vitae sint, sit sequi? Ea voluptatibus facilis velit,
-        nam possimus dolores corporis temporibus nobis sit incidunt, nisi natus
-        voluptates explicabo tenetur maxime recusandae culpa. Libero impedit
-        tenetur quidem, dolor necessitatibus similique enim, consectetur
-        deserunt voluptate doloremque rem odio consequuntur accusamus qui ipsum
-        temporibus illum quis at natus vel explicabo! Facere vero fugiat
-        temporibus harum ipsam pariatur? Consectetur et placeat, quod cum cumque
-        dicta eius est numquam maxime aliquam sequi libero possimus beatae,
-        suscipit odit repellendus earum nihil laudantium quia nobis officiis!
-        Cum ipsum itaque, repellat explicabo quas inventore. Ab animi ea sed sit
-        facilis! Maiores ducimus itaque esse a soluta maxime obcaecati, labore
-        quaerat, harum consequuntur iure ipsam nemo animi voluptates vero non
-        facere iste dolorum quo commodi! Quibusdam, dolorum reiciendis!
-        Accusamus ad vero dicta quam, laudantium sequi doloribus, omnis,
-        distinctio maxime ea exercitationem eius dolore modi rem tempore unde
-        quasi eos beatae nisi. Itaque eos ipsum nemo sit necessitatibus amet?
-        Veniam perferendis, a iure sequi doloribus alias velit impedit, autem
-        deserunt quibusdam doloremque hic, dicta fugiat eligendi dolorum est
-        odio? Quod, animi cupiditate. Unde, earum possimus rerum perferendis
-        laborum sint soluta eius culpa placeat voluptate? Quam, expedita animi
-        assumenda, id natus corporis rerum ipsum adipisci quidem facere impedit
-        eum quod voluptatibus recusandae modi saepe quasi consectetur
-        consequuntur harum deserunt aliquam.
+      {user ? <div>{`Welcome, ${user.name}!`}</div> : <div>WTF?</div>}
+      <div className='latest-items'>
+        <p>Latest 5 items added to the marketplace:</p>
+        {items
+          .sort((a, b) => {
+            return (
+              new Date(b.item.listedAt).valueOf() -
+              new Date(a.item.listedAt).valueOf()
+            );
+          })
+          .splice(0, 5)
+          .map(item => (
+            <div>
+              <p>{item.item.name}</p>
+              <p>{moment(item.item.listedAt).format('DD-MM-YYYY')}</p>
+            </div>
+          ))}
       </div>
     </div>
   );
 };
 
-export default Dashboard;
+const mapStateToProps = (state: StoreState) => ({
+  items: state.items,
+  user: state.user
+});
+
+export default connect(mapStateToProps)(Dashboard);
