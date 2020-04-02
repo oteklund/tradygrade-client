4f4d057e16e8511fdb6ce96e6c98ea9bf33c6f46
Pekka Härkönen <pekka.o.harkonen@gmail.com>
2020-04-02 11:49:14 +0300

    moving to chat by clicking button from item page now works

--- src/components/views/SalesItem.tsx
+++ src/components/views/SalesItem.tsx
@@ -2,20 +2,21 @@
 This component is for viewing and buying an existing item. The owner of the item may also edit item details. For posting a new item see component NewSalesItem.
 */
 
-import "./SalesItem.scss";
-import React, { useState, useEffect } from "react";
-import { StoreState, Item, User } from "../../models/types";
-import { connect } from "react-redux";
-import moment from "moment";
-import history from "../../history";
-import { start } from "repl";
-import { usersReducer } from "../../reducers/users";
+import './SalesItem.scss';
+import React, { useState, useEffect, SyntheticEvent } from 'react';
+import { StoreState, Item, User } from '../../models/types';
+import { connect } from 'react-redux';
+import moment from 'moment';
+import history from '../../history';
+import { start } from 'repl';
+import { usersReducer } from '../../reducers/users';
+import { newChat, getChatID } from '../../services/chat';
 // import { RouteProps } from 'react-router';
 
 interface Props {
   match: any;
   items: Item[];
-  user: any;
+  user: User | any;
 }
 
 const SalesItem = ({ items, match, user }: Props) => {
@@ -29,9 +30,30 @@ const SalesItem = ({ items, match, user }: Props) => {
     //eslint-disable-next-line
   }, [match.params.id]);
 
+  const handleChat = async (e: any) => {
+    if (item && user.id) {
+      try {
+        let chatExists = await getChatID(user.id, item.seller.id);
+        if (!chatExists) {
+          let response = await newChat({
+            user1: user.id,
+            user2: item.seller.id
+          });
+          history.push(`/chat/${response.id}/${item.seller.name}`);
+        } else {
+          history.push(`/chat/${chatExists}/${item.seller.name}`);
+        }
+      } catch (error) {
+        alert(
+          'Something went wrong! Please try to start the chat by clicking the Chat-icon from the navigation!'
+        );
+      }
+    }
+  };
+
   const handleProfileClick = (e: any): void => {
     if (item) {
-      let userUrlParam = item.seller.name.replace(/\s/, "");
+      let userUrlParam = item.seller.name.replace(/\s/, '');
       history.push({
         pathname: `/users/${userUrlParam}`,
         state: { name: item.seller.name }
@@ -42,11 +64,12 @@ const SalesItem = ({ items, match, user }: Props) => {
   const goBack = (e: any): void => {
     history.goBack();
   };
+
   if (item) {
     return (
-      <div className="item-container">
+      <div className='item-container'>
         <h3>{item.item.name}</h3>
-        <div className="item-content">
+        <div className='item-content'>
           <p>Seller</p>
           <div>
             {item.seller.name}
@@ -57,21 +80,21 @@ const SalesItem = ({ items, match, user }: Props) => {
           <p>Condition</p>
           <div>{item.item.condition}</div>
           <p>Listed at</p>
-          <div>{moment(item.item.listedAt).format("DD-MM-YYYY")}</div>
+          <div>{moment(item.item.listedAt).format('DD-MM-YYYY')}</div>
           <p>Price</p>
           {/* <img className="itemPicture" src={item.item.pictureURL} height="200px" /> */}
           <div>
             <b>{`${item.item.price} €`}</b>
           </div>
           {item.seller.name !== user.name ? (
-            <div className="sales-item-buttons-for-buyer">
+            <div className='sales-item-buttons-for-buyer'>
               <button>Buy Item</button>
-              <button>Chat With Seller</button>
+              <button onClick={handleChat}>Chat With Seller</button>
               <br />
               <button onClick={goBack}>Go Back</button>
             </div>
           ) : (
-            <div className="sales-item-buttons-for-seller">
+            <div className='sales-item-buttons-for-seller'>
               <button>Edit</button>
               <button>Delete</button>
               <br />
