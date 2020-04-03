0cdebed6ab983e8ace2cee549dbe2ccb12df78b1
Pekka Härkönen <pekka.o.harkonen@gmail.com>
2020-04-03 09:18:39 +0300

    search function and UI finished for presentation

--- src/components/search-results/UserComponent.tsx
+++ src/components/search-results/UserComponent.tsx
@@ -1,24 +1,30 @@
-import React from 'react';
+import React, { SyntheticEvent } from 'react';
 import history from '../../history';
 import { User } from '../../models/types';
 import '../views/SearchResults.scss';
+import { getChatID, newChat } from '../../services/chat';
 
 interface Props {
   user: User;
 }
 
 const UserComponent = ({ user }: Props) => {
-  const handleProfileClick = (e: any): void => {
-    let userUrlParam = user.name.replace(/\s/, '');
+  const handleChat = async (e: SyntheticEvent) => {
+    console.log(
+      "We didn't have time to implement this feature. It will be here soon!"
+    );
+  };
+  const handleProfileClick = (e: SyntheticEvent): void => {
     history.push({
-      pathname: `/users/${userUrlParam}`,
-      state: { name: user.name }
+      pathname: `/users/${user.name}`
     });
   };
   return (
     <div id='user-list-item' key={user.id}>
       <span>{user.name}</span>
-      <button className='result-button'>Chat</button>
+      <button className='result-button' onClick={handleChat}>
+        Chat
+      </button>
       <button className='result-button' onClick={e => handleProfileClick(e)}>
         View Profile
       </button>
